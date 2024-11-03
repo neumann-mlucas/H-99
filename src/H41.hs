394a9f0fd2c8c71d9daf20e29157d8f8c5598d43
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-11-03 16:28:34 -0300

    H41

---
+++ src/H41.hs
@@ -0,0 +1,17 @@
+isPrime :: Int -> Bool
+isPrime 1 = False
+isPrime n = all ((/=0).(mod n)) [2..n-1]
+
+goldbach :: Int -> (Int, Int)
+goldbach n = head pairs 
+  where
+    primes = filter isPrime [1..n]
+    pairs = [(x,y) | x <- primes, y <- primes, x+y == n] 
+
+goldbachList :: Int -> Int -> [(Int, Int)]
+goldbachList n m = map goldbach list 
+  where 
+  list = filter (not . isPrime) $ filter even [n..m]
+
+main = 
+  print $ goldbachList 4 200
